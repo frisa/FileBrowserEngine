c63f011168ad12c821007f0728fe569b6e00ea74
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-05-01 16:26:40 +0200

    Add the time measured file listing

--- engine.cpp
+++ engine.cpp
@@ -3,36 +3,37 @@
 #include <filesystem>
 #include <vector>
 
-namespace fs = std::filesystem;
-
-void getFiles(std::filesystem::path rootPath, bool toFile=false)
+inline void getFilesFast(std::filesystem::path rootPath)
 {
     std::vector<std::filesystem::directory_entry> output;
-    std::ofstream outFile;
-    if (toFile){
-        outFile.open("/home/jan/Repositories/FileBrowserEngine/output.txt");
-    }
-
-    for (const auto &entry: std::filesystem::recursive_directory_iterator(rootPath))
+    for (const auto &entry : std::filesystem::recursive_directory_iterator(rootPath))
     {
-        //if (toFile) outFile<< entry.path()<<std::endl;
+        // if (toFile) outFile<< entry.path()<<std::endl;
         output.emplace_back(entry);
     }
-    if (toFile){
-        outFile.close();
-    }
-    std::cout<< "Entries: " << output.size()<< std::endl;
+    std::cout << "Entries: " << output.size() << " ";
 }
 
 int main()
 {
     std::cout << "File Browser Engine has started" << std::endl;
-    for(auto i{0}; i<5; i++)
+    const unsigned int count{10};
+    std::chrono::time_point<std::chrono::system_clock> begin;
+    std::chrono::time_point<std::chrono::system_clock> end;
+    uint64_t duration{0};
+    uint64_t sum{0};
+
+    for (auto i{0}; i < count; i++)
     {
-        auto begin = std::chrono::high_resolution_clock::now();
-        getFiles("/home/jan/Repositories/gmat-swx");
-        auto end = std::chrono::high_resolution_clock::now();
-        std::cout<< std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << "[ms]" << std::endl;
+        begin = std::chrono::high_resolution_clock::now();
+        getFilesFast("/home/jan/Repositories/gmat-swx");
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+        std::cout << "Duration: " << duration << std::endl;
+        sum += duration;
     }
+
+    double average_duration = sum / count;
+    std::cout << "Average Duration: " << average_duration << std::endl;
     return 0;
 }
