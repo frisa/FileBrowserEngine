a66d8a54b6ea431301e5f4758c1953127487579a
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-05-01 15:57:13 +0200

    Added simpel listing the directory entries

--- engine.cpp
+++ engine.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <fstream>
 #include <filesystem>
+#include <vector>
 
 namespace fs = std::filesystem;
 
-void getFiles(std::filesystem::path rootPath)
+void getFiles(std::filesystem::path rootPath, bool toFile=false)
 {
-    for (const auto &entry : std::filesystem::directory_iterator(rootPath))
-    {
-        std::cout << entry.path() << std::endl;
+    std::vector<std::filesystem::directory_entry> output;
+    std::ofstream outFile;
+    if (toFile){
+        outFile.open("/home/jan/Repositories/FileBrowserEngine/output.txt");
     }
+
+    for (const auto &entry: std::filesystem::recursive_directory_iterator(rootPath))
+    {
+        //if (toFile) outFile<< entry.path()<<std::endl;
+        output.emplace_back(entry);
+    }
+    if (toFile){
+        outFile.close();
+    }
+    std::cout<< "Entries: " << output.size()<< std::endl;
 }
 
 int main()
 {
     std::cout << "File Browser Engine has started" << std::endl;
-    getFiles("/");
+    for(auto i{0}; i<5; i++)
+    {
+        auto begin = std::chrono::high_resolution_clock::now();
+        getFiles("/home/jan/Repositories/gmat-swx");
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout<< std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << "[ms]" << std::endl;
+    }
     return 0;
 }
